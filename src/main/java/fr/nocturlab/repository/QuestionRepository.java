47cf673a84ab5495ed8e261a53945407207d9e80
Flavien CADET <flavien.cadet@nocturlab.fr>
2019-11-06 17:18:06 +0100

    Ajout de la gestion des difficultés

--- src/main/java/fr/nocturlab/repository/QuestionRepository.java
+++ src/main/java/fr/nocturlab/repository/QuestionRepository.java
@@ -12,9 +12,11 @@ import fr.nocturlab.entities.Question;
 
 @RepositoryRestResource
 public interface QuestionRepository extends CrudRepository<Question, Integer> {
+    // Count the number of questions you still need to answer.
     @Query(value = "SELECT count(*) FROM question q WHERE q.id not in (SELECT distinct r.question_id FROM resultat r WHERE r.account_id = :account)", nativeQuery = true)
     Integer countByNotAlreadyAnswer(@Param("account") Account account);
 
-    @Query(value = "SELECT * FROM question q WHERE q.id not in (SELECT distinct r.question_id FROM resultat r WHERE r.account_id = :account)", nativeQuery = true)
+    // get the 5 question that was closest of the difficulty of the player
+    @Query(value = "SELECT * FROM question q2 WHERE q.id IN (SELECT q.id, abs(q.difficulty - (SELECT difficulty FROM account WHERE id=1)) as difficulty FROM question q WHERE q.id not in (SELECT distinct r.question_id FROM resultat r WHERE r.account_id = 1) ORDER BY difficulty LIMIT 10)", nativeQuery = true)
     List<Question> findByNotAlreadyAnswer(@Param("account") Account account);
 }
